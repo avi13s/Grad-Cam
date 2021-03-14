b53a4abf1b4fb2c77bc0ecb71b0f439265a25dd2
IgorDroz <44924253+IgorDroz@users.noreply.github.com>
2021-03-14 12:46:27 +0200

    Update ResNet34.py

--- ResNet34.py
+++ ResNet34.py
@@ -1,13 +1,9 @@
-# coding:utf8
 from BasicModule import BasicModule
 from torch import nn
 from torch.nn import functional as F
 
 
 class ResidualBlock(nn.Module):
-    '''
-    实现子module: Residual Block
-    '''
 
     def __init__(self, inchannel, outchannel, stride=1, shortcut=None):
         super(ResidualBlock, self).__init__()
@@ -27,24 +23,17 @@ class ResidualBlock(nn.Module):
 
 
 class ResNet34(BasicModule):
-    '''
-    实现主module：ResNet34
-    ResNet34包含多个layer，每个layer又包含多个Residual block
-    用子module来实现Residual block，用_make_layer函数来实现layer
-    '''
 
     def __init__(self, num_classes=2):
         super(ResNet34, self).__init__()
         self.model_name = 'resnet34'
 
-        # 前几层: 图像转换
         self.pre = nn.Sequential(
             nn.Conv2d(3, 64, 7, 2, 3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(3, 2, 1))
 
-        # 重复的layer，分别有3，4，6，3个residual block
         self.layer1 = self._make_layer(64, 128, 3)
         self.layer2 = self._make_layer(128, 256, 4, stride=2)
         self.layer3 = self._make_layer(256, 512, 6, stride=2)
@@ -53,13 +42,10 @@ class ResNet34(BasicModule):
                                         nn.Tanh(),
                                         nn.Conv2d(512,512,3,padding=1))
 
-        # 分类用的全连接
         self.fc = nn.Linear(2048*4, num_classes)
 
     def _make_layer(self, inchannel, outchannel, block_num, stride=1):
-        '''
-        构建layer,包含多个residual block
-        '''
+
         shortcut = nn.Sequential(
             nn.Conv2d(inchannel, outchannel, 1, stride, bias=False),
             nn.BatchNorm2d(outchannel))
